e448e370fc4d9849c8ab0c2ee1eb6ea979600364
roman <roma1808@inbox.ru>
2024-02-21 20:51:29 +0300

    Update main.py

--- document_service/app/main.py
+++ document_service/app/main.py
@@ -24,7 +24,7 @@ keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
                                   realm_name=KEYCLOAK_REALM,
                                   client_secret_key=KEYCLOAK_CLIENT_SECRET)
 
-ser_token = ""
+user_token = ""
 
 
 ###########
@@ -71,49 +71,63 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def doc_health():
-    return {'message': 'service is active'}
+    if (check_user_roles()):
+        return {'message': 'service is active'}
+    else:
+        return "Wrong JWT Token"
 
 
 @app.get("/user_docs")
 async def fetch_docs(db: db_dependency):
-    result = db.query(database.DBDoc).offset(0).limit(100).all()
-    return result
+    if (check_user_roles()):
+        result = db.query(database.DBDoc).offset(0).limit(100).all()
+        return result
+    else:
+        return "Wrong JWT Token"
 
 
 @app.get("/doc_by_id")
 async def fetch_docs(owner_id: UUID, db: db_dependency):
-    result = db.query(database.DBDoc).filter(database.DBDoc.owner_id == owner_id).first()
-    print(owner_id)
-    print(result)
-    if not result:
-        raise HTTPException(
-            status_code=404,
-            detail=f'doc with such owner id is not found. owner_id: {owner_id}'
-        )
-    return result
+    if (check_user_roles()):
+        result = db.query(database.DBDoc).filter(database.DBDoc.owner_id == owner_id).first()
+        print(owner_id)
+        print(result)
+        if not result:
+            raise HTTPException(
+                status_code=404,
+                detail=f'doc with such owner id is not found. owner_id: {owner_id}'
+            )
+        return result
+    else:
+        return "Wrong JWT Token"
 
 
 @app.post('/add_doc')
 async def add_doc(doc: Document, db: db_dependency):
-    db_doc = database.DBDoc(
-        id=doc.id,
-        owner_id=doc.owner_id,
-        title=doc.title,
-        body=doc.body,
-    )
-    db.add(db_doc)
-    db.commit()
-    db.refresh(db_doc)
-    return {"id": doc.id}
-
+    if (check_user_roles()):
+        db_doc = database.DBDoc(
+            id=doc.id,
+            owner_id=doc.owner_id,
+            title=doc.title,
+            body=doc.body,
+        )
+        db.add(db_doc)
+        db.commit()
+        db.refresh(db_doc)
+        return {"id": doc.id}
+    else:
+        return "Wrong JWT Token"
 
 @app.delete("/delete_document")
 async def delete_doc(doc_id: int, db: db_dependency):
-    try:
-        doc_db = db.query(database.DBDoc).filter(database.DBDoc.id == doc_id).first()
-        db.delete(doc_db)
-    except Exception:
-        return "cant find document"
+    if (check_user_roles()):
+        try:
+            doc_db = db.query(database.DBDoc).filter(database.DBDoc.id == doc_id).first()
+            db.delete(doc_db)
+        except Exception:
+            return "cant find document"
+    else:
+        return "Wrong JWT Token"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 80)))
