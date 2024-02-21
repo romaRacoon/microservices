58e6a989c23e4c9d4a40526a0f6dd69ef2a87744
roman <roma1808@inbox.ru>
2024-02-21 20:35:25 +0300

    test

--- document_service/app/main.py
+++ document_service/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import FastAPI, HTTPException, status, Depends, Form
 from typing import Annotated
 from uuid import UUID
 from model.document import Document
@@ -6,12 +6,57 @@ import uvicorn
 import os
 from database import database as database
 from sqlalchemy.orm import Session
+from keycloak import KeycloakOpenID
 
 app = FastAPI()
 
 app = FastAPI()
 database.Base.metadata.create_all(bind=database.engine)
 
+# Данные для подключения к Keycloak
+KEYCLOAK_URL = "http://keycloak:8080/"
+KEYCLOAK_CLIENT_ID = "boyarkov"
+KEYCLOAK_REALM = "myrealm"
+KEYCLOAK_CLIENT_SECRET = "T678RfL6Jxtk5zmNQygPAn7ahcTnPzTr"
+
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                  client_id=KEYCLOAK_CLIENT_ID,
+                                  realm_name=KEYCLOAK_REALM,
+                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
+ser_token = ""
+
+
+###########
+#Prometheus
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
+
+@app.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):
+    try:
+        # Получение токена
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        global user_token
+        user_token = token
+        return token
+    except Exception as e:
+        print(e)  # Логирование для диагностики
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+def check_user_roles():
+    global user_token
+    token = user_token
+    try:
+        userinfo = keycloak_openid.userinfo(token["access_token"])
+        token_info = keycloak_openid.introspect(token["access_token"])
+        if "testRole" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_info
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
 
 def get_db():
     db = database.SessionLocal()
